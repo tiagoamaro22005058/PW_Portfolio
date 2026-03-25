3df39dc1f945635e820bcbfc20eaa406f08e57d0
tiagoamaro22005058 <a22005058@alunos.ulht.pt>
2026-03-25 14:51:41 +0000

    Esquecime de dar save no admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Licenciatura
 
-# Register your models here.
+@admin.register(Licenciatura) # em vez de por admin.site.register(Licenciatura, Licenciatura Admin) <-- Visto no Claude
+class LicenciaturaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sigla', 'grau', 'duracao_anos')
+    search_fields = ('nome', 'sigla')
