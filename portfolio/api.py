c4a99eb6bcd5053d2ed737d47b0361c5f426d68e
tiagoamaro22005058 <a22005058@alunos.ulht.pt>
2026-05-30 15:29:53 +0100

    API de Porjetos e Tecnologias ja estao a funcinar, mas a mensagem de sucesso nao estar a aparecer

--- portfolio/api.py
+++ portfolio/api.py
@@ -1,7 +1,7 @@
 from ninja import NinjaAPI
-from .schemas import UnidadeCurricularOut, ErrorSchema, UnidadeCurricularIn
+from .schemas import UnidadeCurricularOut, ErrorSchema, UnidadeCurricularIn, ProjetoOut, ProjetoIn, TecnologiaOut, TecnologiaIn
 from typing import List
-from .models import UnidadeCurricular
+from .models import UnidadeCurricular, Projeto, Tecnologia
 
 
 api = NinjaAPI(
@@ -58,7 +58,7 @@ def atualizar_UnidadeCurricular(request, unidade_id: int, data: UnidadeCurricula
     for attr, value in data.dict().items():
         setattr(unidade, attr, value)
     unidade.save()
-    return 200, unidade
+    return 200, unidade  #nao sei como por mensagem de sucesso
 
 
 #apagar unidadeCurricular
@@ -76,3 +76,132 @@ def apagar_UnidadeCurricular(request, unidade_id: int):
     return 204, {"messagem": "Unidade Curricular excluída com sucesso"}
 
 
+####Projetos####
+
+#listar Projetos
+@api.get("projetos/",
+         response={200: List[ProjetoOut]},
+         tags=["Projetos"],
+         description="Lista todos os projetos"
+        )
+def lista_Projetos(request):
+    return 200, Projeto.objects.select_related("uc").all()
+
+
+#ver projeto por id
+@api.get("projetos/{projeto_id}",
+         response={200: ProjetoOut, 404: ErrorSchema},
+         tags=["Projetos"],
+         description="Ver detalhes de um projeto por id"
+         )
+def ver_Projeto(request, projeto_id: int):
+    try:
+        return 200, Projeto.objects.select_related("uc").get(id=projeto_id)
+    except Projeto.DoesNotExist:
+        return 404, {"messagem": "Projeto não encontrado"}
+   
+    
+#criar unidadeCurricular
+@api.post("projetos/",
+          response={201: ProjetoOut},
+          tags=["Projetos"],
+          description="Criar um novo projeto"
+          )
+def criar_Projeto(request, data: ProjetoIn):
+    return 201, Projeto.objects.create(**data.dict())    
+
+
+#atualizar projectos
+@api.put("projetos/{projeto_id}",
+         response={200: ProjetoOut, 404: ErrorSchema},
+         tags=["Projetos"],
+         description="Atualizar um projeto por id"
+        )
+def atualizar_Projeto(request, projeto_id: int, data: ProjetoIn):
+    try:
+        projeto = Projeto.objects.get(id=projeto_id)
+    except Projeto.DoesNotExist:
+        return 404, {"messagem": "Projeto não encontrado"}
+    for attr, value in data.dict().items():
+        setattr(projeto, attr, value)
+    projeto.save()
+    return 200, projeto  #nao sei como por mensagem de sucesso  
+
+
+#apagar projeto
+@api.delete("projetos/{projeto_id}",
+            response={204: None, 404: ErrorSchema},
+            tags=["Projetos"],
+            description="Excluir um projeto por id"
+            )
+def apagar_Projeto(request, projeto_id: int):
+    try:
+        projeto = Projeto.objects.get(id=projeto_id)
+    except Projeto.DoesNotExist:
+        return 404, {"messagem": "Projeto não encontrado"}
+    projeto.delete()
+    return 204, {"messagem": "Projeto excluído com sucesso"}
+
+
+####Projetos####
+
+#listar tecnologias
+@api.get("tecnologias/",
+         response={200: List[TecnologiaOut]},
+         tags=["Tecnologias"],
+         description="Lista todas as tecnologias"
+        )
+def lista_Tecnologias(request):
+    return 200, Tecnologia.objects.all()
+
+#ver tecnologia por id
+@api.get("tecnologias/{tecnologia_id}",
+         response={200: TecnologiaOut, 404: ErrorSchema},
+         tags=["Tecnologias"],
+         description="Ver detalhes de uma tecnologia por id"
+         )
+def ver_Tecnologia(request, tecnologia_id: int):
+    try:
+        return 200, Tecnologia.objects.get(id=tecnologia_id)
+    except Tecnologia.DoesNotExist:
+        return 404, {"messagem": "Tecnologia não encontrada"}   
+    
+    
+#criar tecnologia
+@api.post("tecnologias/",
+          response={201: TecnologiaOut},
+          tags=["Tecnologias"],
+          description="Criar uma nova tecnologia"
+          )
+def criar_Tecnologia(request, data: TecnologiaIn):
+    return 201, Tecnologia.objects.create(**data.dict())    
+
+#atualizar tecnologia
+@api.put("tecnologias/{tecnologia_id}",
+         response={200: TecnologiaOut, 404: ErrorSchema},
+         tags=["Tecnologias"],
+         description="Atualizar uma tecnologia por id"
+        )
+def atualizar_Tecnologia(request, tecnologia_id: int, data: TecnologiaIn):
+    try:
+        tecnologia = Tecnologia.objects.get(id=tecnologia_id)
+    except Tecnologia.DoesNotExist:
+        return 404, {"messagem": "Tecnologia não encontrada"}
+    for attr, value in data.dict().items():
+        setattr(tecnologia, attr, value)
+    tecnologia.save()
+    return 200, tecnologia  #nao sei como por mensagem de sucesso   
+
+#apagar tecnologia
+@api.delete("tecnologias/{tecnologia_id}",
+            response={204: None, 404: ErrorSchema},
+            tags=["Tecnologias"],
+            description="Excluir uma tecnologia por id"
+            )
+def apagar_Tecnologia(request, tecnologia_id: int):     
+    try:
+        tecnologia = Tecnologia.objects.get(id=tecnologia_id)
+    except Tecnologia.DoesNotExist:
+        return 404, {"messagem": "Tecnologia não encontrada"}
+    tecnologia.delete()
+    return 204, {"messagem": "Tecnologia excluída com sucesso"}
--- portfolio/schemas.py
+++ portfolio/schemas.py
@@ -1,6 +1,8 @@
 from ninja import Schema
 from typing import List, Optional
 
+
+#====Unidade Curricular====
 class UnidadeCurricularOut(Schema):
       id: int
       nome: str
@@ -9,9 +11,8 @@ class UnidadeCurricularOut(Schema):
       semestre: int
       ects: int
       concluida: bool
-      licenciatura_id: int
-
-    
+      licenciatura_id: int = 1
+      
 class UnidadeCurricularIn(Schema):
       nome: str
       sigla: str
@@ -19,9 +20,46 @@ class UnidadeCurricularIn(Schema):
       semestre: int
       ects: int
       concluida: bool
-      licenciatura_id: int
+      licenciatura_id: int = 1
+      
+#====Projeto====
+class ProjetoOut(Schema):
+      id: int
+      titulo: str
+      descricao: str
+      nota: Optional[int]
+      ano_realizacao: int
+      uc_id: int
+
+class ProjetoIn(Schema):
+      titulo: str
+      descricao: str
+      nota: Optional[int] = None
+      ano_realizacao: int
+      uc_id: int
+      
+#====Tecnologia====      
+class TecnologiaOut(Schema):
+    id: int
+    nome: str
+    descricao: Optional[str] = None
+    url_website: Optional[str] = 'https://example.com'
+    nivel_interesse: int = 1
+    categoria: str = 'outro'
+      
+class TecnologiaIn(Schema):
+    nome: str
+    descricao: Optional[str] = None
+    url_website: Optional[str] = 'https://example.com'
+    nivel_interesse: int = 1
+    categoria: str = 'outro'    
+      
+      
       
       
       
 class ErrorSchema(Schema):
     messagem: str
+    
+    
+    
