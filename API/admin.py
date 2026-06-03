aaeb7a11be4564f6cebc3978ddd6ca16e803d947
tiagoamaro22005058 <a22005058@alunos.ulht.pt>
2026-06-03 15:14:40 +0100

    add Noticia model admin with display and search fields

--- API/admin.py
+++ API/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import APIKey
+from .models import APIKey, Noticia
 
 @admin.register(APIKey)
 class APIKeyAdmin(admin.ModelAdmin):    
     list_display = ('name', 'key', 'is_active', 'expiration_date', 'created_at')
     list_filter = ('is_active', 'expiration_date')
     search_fields = ('name', 'key')
+    
+    
+@admin.register(Noticia)
+class NoticiaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'resumo', 'data_publicacao')
+    search_fields = ('titulo', 'resumo')
